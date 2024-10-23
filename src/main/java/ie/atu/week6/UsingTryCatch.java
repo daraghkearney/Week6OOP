b49f5cf90542615bbcb5745ea3e0264984b8f968
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2024-10-23 17:09:12 +0100

    added Retry on Error

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -6,23 +6,25 @@ public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9 :");
         Scanner myscan = new Scanner(System.in);
+        boolean validInput = false;
 
-        try {
-            String input = myscan.nextLine();
+        while (!validInput) {
+            try {
+                String input = myscan.nextLine();
 
 
-            int myNum = Integer.parseInt(input);
+                int myNum = Integer.parseInt(input);
 
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("You entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (NumberFormatException ae) {
+                System.out.println("Input is not a valid number. Please enter a number between 0 - 9 :");
             }
-        } catch (NumberFormatException ae)
-                    {
-            System.out.println("Input is not a valid number");
-        } finally {
-            myscan.close();
+        }
+                myscan.close();
         }
     }
-}
+
